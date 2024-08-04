d23457e8a13d7b4ad949d712db3d2cfb0387c184
caringallawrence <caringallawrence5@gmail.com>
2024-08-04 21:46:21 +1200

    initsss privateee

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://internal-apituan-785481583.ap-southeast-1.elb.amazonaws.com/api"
+  apiUrl: "https://back.lawrence-kps.online/api"
 };
